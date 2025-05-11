001dc0c53bb4e54e4e99f996321895b851d2ca7f
jefersonyanac29 <jefersonyanac29@gmail.com>
2025-05-11 16:59:49 -0500

    feat(swagger): add swagger documentation with dark theme
    Added Swagger documentation for the API, including a custom dark theme for better readability. Removed outdated e2e test files as they are no longer needed with the new documentation setup.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const customCss = readFileSync(
+    join(__dirname, '../css/swagger-dark.css'),
+    'utf8',
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('AttackTracer API')
+    .setDescription('API documentation for AttackTracer Backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customCss,
+    customSiteTitle: 'AttackTracer API Documentation'
+  });
 
   app.enableCors({
     origin: 'http://localhost:3001',
